56e21d8967978db66f2a9f51a7d3462747e538d9
Tamim379ta <tamimthasan55@gmail.com>
2026-08-06 16:32:47 +0600

    edit auth flow

--- config/auth.js
+++ config/auth.js
@@ -2,7 +2,6 @@ const { betterAuth } = require("better-auth");
 const { mongodbAdapter } = require("better-auth/adapters/mongodb");
 const { MongoClient } = require("mongodb");
 
-
 const client = new MongoClient(process.env.MONGO_URI);
 const db = client.db("sparkfund");
 
@@ -45,3 +44,5 @@ const auth = betterAuth({
     },
   },
 });
+
+module.exports = auth;
